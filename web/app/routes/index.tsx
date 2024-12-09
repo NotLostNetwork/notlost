0286dd42a6850b9a7b5268cfae7fa3e21bb78530
Nikita <github@nikiv.dev>
2024-12-09 16:26:37 +0100

    first design

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -2,8 +2,37 @@ import { createFileRoute } from "@tanstack/react-router"
 
 function RouteComponent() {
   return (
-    <div className="text-red-500">
-      App available at <a href="https://t.me/not_lost_bot">@not_lost_bot</a>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        textAlign: "center",
+        backgroundColor: "#1e1e1e",
+        padding: "20px",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "1.5em",
+          color: "#f0f0f0",
+          fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+          textShadow: "1px 1px 2px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        App available at{" "}
+        <a
+          href="https://t.me/not_lost_bot"
+          style={{
+            color: "#4da6ff",
+            textDecoration: "none",
+            fontWeight: "bold",
+          }}
+        >
+          @not_lost_bot
+        </a>
+      </div>
     </div>
   )
 }
