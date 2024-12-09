f5d7bc2c92aad8f565eeb7894f510c34028533ec
Nikita <github@nikiv.dev>
2024-12-09 15:09:15 +0100

    remove https from vinxi

--- web/app/routes/_pages.tsx
+++ web/app/routes/_pages.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
-import '@/shared/styles/app.css'
-import TelegramProvider from '~/shared/lib/telegram/telegram-provider'
+import { createFileRoute, Outlet } from "@tanstack/react-router"
+import "@/shared/styles/app.css"
+import TelegramProvider from "~/shared/lib/telegram/telegram-provider"
 
-export const Route = createFileRoute('/_pages')({
+export const Route = createFileRoute("/_pages")({
   component: RouteComponent,
 })
 
