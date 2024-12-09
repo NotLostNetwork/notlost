9b34f799b0392fd189998542337fb383357edd4a
Nikita <github@nikiv.dev>
2024-12-09 15:43:21 +0100

    build

--- web/app.config.ts
+++ web/app.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from '@tanstack/start/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/start/config"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   vite: {
     ssr: {
-      noExternal: ['@telegram-apps/telegram-ui'],
+      noExternal: ["@telegram-apps/telegram-ui"],
     },
     plugins: [tsconfigPaths()],
-  },
-  server: {
-    preset: 'vercel',
   },
 })
