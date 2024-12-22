4f8c0c92a06643f6e9b1d4822b0334ef68271241
Nikita <github@nikiv.dev>
2024-12-22 17:13:50 +0100

    jazz

--- web/app/shared/lib/jazz/schema/index.ts
+++ web/app/shared/lib/jazz/schema/index.ts
@@ -17,35 +17,40 @@ class Contact extends CoMap {
 }
 class ListOfContacts extends CoList.Of(co.ref(Contact)) {}
 
-class UserProfile extends CoMap {
-  telegramId = co.number
+// account root is an app-specific per-user private `CoMap`
+// where you can store top-level objects for that user
+export class JazzNotLostAccountRoot extends CoMap {
+  telegramId = co.number // unique id for the user (how auth is done kind of)
+  contacts = co.ref(ListOfContacts)
+
   username = co.string
   firstName = co.string
   lastName = co.string
   telegramSync = co.boolean // if true, app will use live tg contact to sync up
-  contacts = co.ref(ListOfContacts)
 }
 
-// account root is an app-specific per-user private `CoMap`
-// where you can store top-level objects for that user
-export class JazzNotLostAccountRoot extends CoMap {}
+export class JazzNotLostAccount extends Account {
+  profile = co.ref(Profile)
+  root = co.ref(JazzNotLostAccountRoot)
 
-// export class JazzNotLostAccount extends Account {
-//   profile = co.ref(Profile)
-//   // root = co.ref(TodoAccountRoot)
-
-//   /** The account migration is run on account creation and on every log-in.
-//    *  You can use it to set up the account root and any other initial CoValues you need.
-//    */
-//   migrate(this: TodoAccount, creationProps?: { name: string }) {
-//     super.migrate(creationProps)
-//     if (!this._refs.root) {
-//       this.root = TodoAccountRoot.create(
-//         {
-//           projects: ListOfProjects.create([], { owner: this }),
-//         },
-//         { owner: this },
-//       )
-//     }
-//   }
-// }
+  /** The account migration is run on account creation and on every log-in.
+   *  You can use it to set up the account root and any other initial CoValues you need.
+   */
+  migrate(this: JazzNotLostAccount, creationProps?: { name: string }) {
+    super.migrate(creationProps)
+    if (!this._refs.root) {
+      this.root = JazzNotLostAccountRoot.create(
+        {
+          // TODO: looks off, not sure what these values should be, especially telegramId
+          contacts: ListOfContacts.create([], { owner: this }),
+          telegramId: 0,
+          username: "",
+          firstName: "",
+          lastName: "",
+          telegramSync: false,
+        },
+        { owner: this },
+      )
+    }
+  }
+}
