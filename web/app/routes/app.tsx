6df3b33fd2ac96b70a0ee7d0b77b18982b9824da
Nikita <github@nikiv.dev>
2024-12-22 17:21:51 +0100

    jazz

--- web/app/routes/app.tsx
+++ web/app/routes/app.tsx
@@ -1,21 +1,29 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router"
 import "@/shared/styles/app.css"
 import TelegramProvider from "~/shared/lib/telegram/telegram-provider"
-import { JazzProvider } from "~/shared/lib/jazz/jazz-provider"
+import { DemoAuthBasicUI, createJazzReactApp, useDemoAuth } from "jazz-react"
+
+const Jazz = createJazzReactApp()
+export const { useAccount, useCoState } = Jazz
 
 export const Route = createFileRoute("/app")({
   component: RouteComponent,
 })
 
-// TODO: FOR TESTING - REMOVE IN PROD
+// TODO: will be removed when beta release, here for dev tools debugging
+// TODO: can do smarter and only show it even in prod for specific tg usernames
 import("eruda").then((lib) => lib.default.init()).catch(console.error)
 
 function RouteComponent() {
+  // TODO: seems we have to edit it as we don't need the `sign up / sign in` scree
+  // sign up / sign in is done in background through telegram id
+  const [auth, state] = useDemoAuth()
+
   return (
     <TelegramProvider>
-      <JazzProvider>
+      <Jazz.Provider auth={auth} peer={import.meta.env.VITE_JAZZ_PEER_URL}>
         <Outlet />
-      </JazzProvider>
+      </Jazz.Provider>
     </TelegramProvider>
   )
 }
