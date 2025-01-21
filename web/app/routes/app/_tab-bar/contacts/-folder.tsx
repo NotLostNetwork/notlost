0cb65e0cce8c417b99acfbeb792f28093457ac91
Shestoy06 <pochta5550@gmail.com>
2025-01-21 01:56:06 +0100

    folder component ui

--- web/app/routes/app/_tab-bar/contacts/-folder.tsx
+++ web/app/routes/app/_tab-bar/contacts/-folder.tsx
@@ -1,60 +1,37 @@
 import { JazzFolder } from "~/lib/jazz/schema"
 import FolderIcon from "@/assets/icons/folder.svg?react"
 import { useState, useRef, useEffect } from "react"
-import { Icon28CloseAmbient } from "@telegram-apps/telegram-ui/dist/icons/28/close_ambient"
-import { Tappable } from "@telegram-apps/telegram-ui"
+import { Icon16Cancel } from "@telegram-apps/telegram-ui/dist/icons/16/cancel"
+import { Accordion } from "@telegram-apps/telegram-ui"
 import { jazzDeleteFolder } from "~/lib/jazz/actions/jazz-folder"
 import { useJazzProfile } from "~/lib/jazz/hooks/use-jazz-profile"
-import { motion, Variants } from "framer-motion"
+import { motion } from "framer-motion"
+import { AccordionContent } from "@telegram-apps/telegram-ui/dist/components/Blocks/Accordion/components/AccordionContent/AccordionContent"
+import { AccordionSummary } from "@telegram-apps/telegram-ui/dist/components/Blocks/Accordion/components/AccordionSummary/AccordionSummary"
+import { InlineButtonsItem } from "@telegram-apps/telegram-ui/dist/components/Blocks/InlineButtons/components/InlineButtonsItem/InlineButtonsItem"
+import PencilIcon from "@/assets/icons/pencil-icon.svg?react"
+import ConfirmModal from "~/ui/modals/confirm-modal"
 
 export const Folder = ({ folder }: { folder: JazzFolder | null }) => {
   const jazzProfile = useJazzProfile()
 
-  const [showDeleteOption, setShowDeleteOption] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(false)
   const [folderTitle, setFolderTitle] = useState(folder?.title || "")
-  const [inputWidth, setInputWidth] = useState(0)
-  const spanRef = useRef<HTMLSpanElement | null>(null)
+  const [isEditTitle, setIsEditTitle] = useState(false)
 
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false)
 
-  const handleTouchStart = () => {
-    timeoutRef.current = setTimeout(() => {
-      setShowDeleteOption((prev) => !prev)
-    }, 500)
-  }
-
-  const handleTouchEnd = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current)
-    }
-  }
-
-  const handleTouchCancel = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current)
-    }
-  }
+  const inputRef = useRef<HTMLInputElement | null>(null)
 
   const deleteFolder = () => {
     if (jazzProfile && folder) {
       jazzDeleteFolder(jazzProfile, folder)
-      setShowDeleteOption(false)
+      setIsExpanded(false)
     }
   }
 
-  const variants: Variants = {
-    static: { scale: 1, rotate: 0 },
-    shake: {
-      rotate: [0, -0.5, 0.5, -0.5, 0.5, 0],
-      transition: {
-        duration: 0.5,
-        repeat: Infinity,
-        repeatType: "loop",
-      },
-    },
-  }
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsEditTitle(true)
     setFolderTitle(e.target.value)
   }
 
@@ -63,73 +40,108 @@ export const Folder = ({ folder }: { folder: JazzFolder | null }) => {
   }, [folder])
 
   useEffect(() => {
-    if (spanRef.current) {
-      setInputWidth(spanRef.current.offsetWidth)
+    if (inputRef.current && isEditTitle) {
+      inputRef.current.focus()
     }
   }, [folderTitle])
 
   return (
     <div className="overflow-hidden no-select">
       <motion.div
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
-        onTouchCancel={handleTouchCancel}
-        variants={variants}
-        animate={showDeleteOption ? "shake" : "static"}
         className="px-4 py-1 overflow-hidden"
         style={{ overflow: "hidden", maxWidth: "100%" }}
       >
-        <div className="rounded-xl bg-secondary flex gap-2 items-center">
-          <div
-            onClick={() => {
-              if (showDeleteOption) {
-                setShowDeleteOption(false)
-              }
-            }}
-            className="flex flex-1 h-full px-4 py-4 rounded-xl gap-2"
-          >
-            <div className="h-6 w-6 text-link">
-              <FolderIcon />
-            </div>
-            <input
-              type="text"
-              value={folderTitle}
-              onChange={handleChange}
-              className="bg-transparent border-none outline-none font-medium z-10"
-              style={{
-                background: "transparent",
-                border: "none",
-                outline: "none",
-                textAlign: "left",
-                width: `${inputWidth}px`,
-              }}
-              onBlur={() => {
-                if (folder) {
-                  if (folderTitle.length === 0) {
-                    folder.title = "New folder"
-                  } else {
-                    folder.title = folderTitle
+        <Accordion
+          expanded={isExpanded}
+          onChange={() => {
+            setIsExpanded((prev) => !prev)
+          }}
+        >
+          <AccordionSummary className="rounded-xl bg-secondary">
+            <div className="flex gap-2 items-center">
+              <div className="h-6 w-6 text-link">
+                <FolderIcon />
+              </div>
+              <input
+                type="text"
+                ref={inputRef}
+                value={folderTitle}
+                onChange={handleChange}
+                className="bg-transparent border-none outline-none font-semibold z-10"
+                style={{
+                  background: "transparent",
+                  border: "none",
+                  outline: "none",
+                  textAlign: "left",
+                }}
+                onBlur={() => {
+                  setIsEditTitle(false)
+                  if (folder) {
+                    if (folderTitle.length === 0) {
+                      folder.title = "New folder"
+                    } else {
+                      folder.title = folderTitle
+                    }
                   }
-                }
-              }}
-            />
-            <span ref={spanRef} className="absolute invisible font-medium">
-              {/* without adding 5 symbols to shadow span for calculate width, input is lagging, don't ask XD */}
-              {folderTitle + "*****" || " "}
-            </span>
-          </div>
-          {showDeleteOption && (
-            <Tappable
-              onClick={() => {
-                deleteFolder()
-              }}
-              className={`ml-auto absolute mr-2 h-full rounded-full`}
-            >
-              <Icon28CloseAmbient />
-            </Tappable>
-          )}
-        </div>
+                }}
+              />
+            </div>
+          </AccordionSummary>
+          <AccordionContent>
+            <div className="flex gap-4">
+              <InlineButtonsItem mode="plain" text="Icon (soon)">
+                <div className="h-5 w-5">
+                  <FolderIcon />
+                </div>
+              </InlineButtonsItem>
+              <InlineButtonsItem
+                onClick={() => {
+                  if (inputRef.current) {
+                    inputRef.current.focus()
+                  }
+                }}
+                mode="plain"
+                text="Rename"
+              >
+                <div className="h-5 w-5">
+                  <PencilIcon />
+                </div>
+              </InlineButtonsItem>
+              <InlineButtonsItem
+                onClick={() => setIsOpenDeleteModal(true)}
+                mode="plain"
+                text="Delete"
+              >
+                <div className="h-5 w-5 flex items-center justify-center">
+                  <Icon16Cancel />
+                </div>
+              </InlineButtonsItem>
+            </div>
+            <div className="p-4">
+              <div>
+                <img
+                  loading="lazy"
+                  src={`https://t.me/i/userpic/320/${"shestaya_liniya"}.svg`}
+                  className="h-12 w-12 rounded-full "
+                  decoding="async"
+                  alt=""
+                />
+                <span
+                  className={`px-2 py-[0.5px] text-xs font-normal bg-buttonBezeled text-link rounded-xl`}
+                >
+                  Andrei
+                </span>
+              </div>
+            </div>
+          </AccordionContent>
+        </Accordion>
       </motion.div>
+      <ConfirmModal
+        isOpen={isOpenDeleteModal}
+        closeModal={() => setIsOpenDeleteModal(false)}
+        title="Remove folder with dialogs?"
+        action={deleteFolder}
+      />
     </div>
   )
 }
